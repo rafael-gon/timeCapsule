c430483099b3d8753ed634e55a6cff580244a4ce
Rafael Goncalves <54370545+rafael-gon@users.noreply.github.com>
2023-10-25 21:01:31 -0300

    Update

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = 'mongodb+srv://rafael:ZmPtWF1Z2kovQQi0@cluster.dkpt9x8.mongodb.net/memories?retryWrites=true&w=majority'
+const db = '[your mongoDB collection]'
 
 const connection = mongoose.connect(db, {
   useNewUrlParser: true,
